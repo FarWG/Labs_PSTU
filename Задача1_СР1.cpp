2697ed1c0c6007d236d573f60391d27b7800d918
RuslanAkhunov <90615254+FarWG@users.noreply.github.com>
2021-10-25 23:08:21 +0500

    Add files via upload

---
+++ Задача1_СР1.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int x;
+	int a, b, c;
+	bool F = false;
+	int t;
+	cout << "Введите 3-х значное число" << endl;
+	cin >> x;
+	while ((x < 100) || (x > 999)) {
+		cout << "Вы ввели неправильно число, повторите попытку" << endl;
+		cin >> x;
+	}
+	a = x % 10;
+	x = x / 10;
+	b = x % 10;
+	c = x / 10;
+	while (F == false) {
+		if ((a == b) || (a == c) || (b == c)) {
+			cout << "Вы ввели число, в котором повторяются цифры, повторите попытку" << endl;
+			cin >> x;
+			a = x % 10;
+			x = x / 10;
+			b = x % 10;
+			c = x / 10;
+		}
+		else {
+			F = true;
+		}
+	}
+	cout << "Возможные варианты:" << endl;
+	cout << a << b << c << endl;
+	t = c;
+	c = b;
+	b = t;
+	cout << a << b << c << endl;
+	t = a;
+	a = c;
+	c = b;
+	b = t;
+	cout << a << b << c << endl;
+	t = c;
+	c = b;
+	b = t;
+	cout << a << b << c << endl;
+	t = a;
+	a = b;
+	b = c;
+	c = t;
+	cout << a << b << c << endl;
+	t = c;
+	c = b;
+	b = t;
+	cout << a << b << c << endl;
+	cout << "Максимальное число:" << endl;
+	if ((a > b) && (a > c)) {
+		if (b > c) {
+			cout << a << b << c << endl;
+		}
+		else {
+			cout << a << c << b << endl;
+		}
+	}
+	if ((b > a) && (b > c)) {
+		if (a > c) {
+			cout << b << a << c << endl;
+		}
+		else {
+			cout << b << c << a << endl;
+		}
+	}
+	if ((c > a) && (c > b)) {
+		if (a > b) {
+			cout << c << a << b << endl;
+		}
+		else {
+			cout << c << b << a << endl;
+		}
+	}
+	return 0;
+}
---
+++ Задача2_СР1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int num, u, i;
+	num = rand() % 101;
+	i = 0;
+	bool f = true;
+	while ((i <= 6) && (f == true)) {
+		i += 1;
+		cout << i << " попытка" << endl;
+		cout << "Попытайтесь угадать число:" << endl;
+		cin >> u;
+		if (u < num) {
+			cout << "Данное число меньше, чем загадонное" << endl;
+		}
+		if (u > num) {
+			cout << "Данное число больше, чем загадонное" << endl;
+		}
+		if (u == num) {
+			f = false;
+			cout << "Число угадано" << endl;
+		}
+	}
+	if (i == 7) {
+		cout << "Попытки закончились" << endl;
+	}
+	// cout << num;
+	return 0;
+}
+
