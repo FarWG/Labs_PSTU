98623b360a7a372386c03e19c1e51a6676af7b33
RuslanAkhunov <90615254+FarWG@users.noreply.github.com>
2021-11-02 22:21:48 +0500

    Add files via upload

---
+++ Задача3_СР4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream> // Директива для добавления библиотеки iostream
+
+using namespace std; // Объявления пространства имен std
+
+int main() // Обязательная функия main
+{
+    setlocale(LC_ALL, "Russian"); // Функция для Русского языка при выводе в консоле
+    const int a = 10; // Целая константа
+    int b, c;
+    c = 20;
+    cin >> b; // Ввод b
+    cout << "Вывод:" << endl;
+    cout << a << endl;
+    cout << b << endl;
+    cout << --c << endl; // Операция декремента
+}
