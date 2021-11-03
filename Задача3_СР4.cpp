76c545b9c500674cf45ee1ac85e9f1b48ab5ca3e
RuslanAkhunov <90615254+FarWG@users.noreply.github.com>
2021-11-03 07:51:06 +0500

    Update Задача3_СР4.cpp

--- Задача3_СР4.cpp
+++ Задача3_СР4.cpp
@@ -6,11 +6,10 @@ int main() // Обязательная функия main
 {
     setlocale(LC_ALL, "Russian"); // Функция для Русского языка при выводе в консоле
     const int a = 10; // Целая константа
-    int b, c;
-    c = 20;
+    int b;
     b = a * 2; // Присваивание b
     cout << "Вывод:" << endl;
     cout << a << endl;
     cout << b << endl;
-    cout << --c << endl; // Операция декремента
+    cout << --b << endl; // Операция декремента
 }
