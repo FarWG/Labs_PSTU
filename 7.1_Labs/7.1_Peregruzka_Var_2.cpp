a07a712b999afcce28224c0a62e1eb3a431c0f6e
RuslanAkhunov <90615254+FarWG@users.noreply.github.com>
2022-02-08 17:47:55 +0500

    Add files via upload

---
+++ 7.1_Labs/7.1_Peregruzka_Var_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <complex>
+
+using namespace std;
+
+float summa(float a, float b)
+{
+	return a + b;
+}
+
+complex<double>summa(complex<double>a, complex<double>b)
+{
+	return a + b;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Сумма комплескных чисел равна: " << summa(complex<double>(4, 6), complex<double>(3, 5)) << endl;
+	cout << "Сумма вещественных чисел равна: " << summa(983.4, 746);
+	return 0;
+}
