d2ae07bc98745a50f2d747718dbb854805faf451
RuslanAkhunov <90615254+FarWG@users.noreply.github.com>
2022-03-04 19:13:37 +0500

    Add files via upload

---
+++ 9_Labs/9_Labs.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string tmp;
+    fstream F1("F1.txt", ios::in);
+    fstream F2("F2.txt", ios::out);
+    while (getline(F1, tmp))
+    {
+        if (tmp.front() == 'А')
+        {
+            F2 << tmp << endl;
+        }   
+    }
+    F1.close();
+    F2.close();
+    return 0;
+}
