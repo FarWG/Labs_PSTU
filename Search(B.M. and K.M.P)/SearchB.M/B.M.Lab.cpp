b1afdbb4b41b3d6ba937d64a7948369229196ce7
RuslanAkhunov <90615254+FarWG@users.noreply.github.com>
2022-03-22 15:38:54 +0500

    Add files via upload

---
+++ Search(B.M. and K.M.P)/SearchB.M/B.M.Lab.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Search(string str, string sbstr)
+{
+	int st = str.size();
+	int sbst = sbstr.size();
+	int res = -1;
+	if (st != 0 && sbst != 0)
+	{
+		int pos;
+		int b[256];
+		for (int i = 0; i < 256; i++)
+		{
+			b[i] = sbst;
+		}
+		for (int i = sbst - 2; i >= 0; i--)
+		{
+			if (b[int((unsigned char)sbstr[i])] == sbst)
+			{
+				b[int((unsigned char)sbstr[i])] = sbst - i - 1;
+			}
+		}
+		pos = sbst - 1;
+		while (pos < st)
+		{
+			if (sbstr[sbst - 1] != str[pos])
+			{
+				pos += b[int((unsigned char)str[pos])];
+			}
+			else
+			{
+				for (int i = sbst - 1; i >= 0; i--)
+				{
+					if (sbstr[i] != str[pos - sbst + i + 1])
+					{
+						pos += b[int((unsigned char)str[pos - sbst + i + 1])];
+						break;
+					}
+					else if (i == 0)
+					{
+						return pos - sbst + 1;
+					}
+				}
+			}
+		}
+	}
+	return res;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string st;
+	string sbst;
+	cout << "Введите строку: ";
+	cin >> st;
+	int n = st.size();
+	string* st2 = new string[n];
+	cout << "Введите подстроку: ";
+	cin >> sbst;
+	int ist = Search(st, sbst);
+	if (ist == -1)
+	{
+		cout << "Не найден";
+		return 0;
+	}
+	for (int i = 0; i < ist; i++)
+	{
+		*st2 += st[i];
+	}
+	int size = sbst.size();
+	int i = ist;
+	while (st[i + size] != '\0')
+	{
+		*st2 += st[i + size];
+		i++;
+	}
+	cout << "Итоговая строка: ";
+	cout << *st2;
+	delete[]st2;
+	return 0;
+}
---
+++ Search(B.M. and K.M.P)/SearchK.M.P/K.M.P.Lab.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Search(string str, string sbstr)
+{
+	int st, sbst;
+	st = str.size();
+	sbst = sbstr.size();
+	int* d = new int[sbst];
+	int i = 0;
+	int j = -1;
+	d[0] = -1;
+	while (i < sbst - 1)
+	{
+		while ((j >= 0) && (sbstr[j] != sbstr[i]))
+		{
+			j = d[j];
+		}
+		i++;
+		j++;
+		if (sbstr[i] == sbstr[j])
+		{
+			d[i] = d[j];
+		}
+		else
+		{
+			d[i] = j;
+		}
+		i = 0;
+		j = 0;
+		for (i = 0, j = 0; (i <= st - 1) && (j <= sbst - 1); i++, j++)
+		{
+			while ((j >= 0) && (sbstr[j] != str[i]))
+			{
+				j = d[j];
+			}
+		}
+		delete[] d;
+		if (j == sbst)
+		{
+			return i - j;
+		}
+		else return -1;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string st;
+	cout << "Введите строку: ";
+	cin >> st;
+	string sbst;
+	string* st2 = new string[st.size()];
+	cout << "Введите подстроку: ";
+	cin >> sbst;
+	int ist = Search(st, sbst);
+	if (ist == -1)
+	{
+		cout << "Не найден";
+		return 0;
+	}
+	int size = sbst.size();
+	for (int i = 0; i < size; i++)
+	{
+		*st2 += sbst[i];
+	}
+	for (int i = 0; i < ist; i++)
+	{
+		*st2 += st[i];
+	}
+	int i = ist;
+	while (st[i + size] != '\0')
+	{
+		*st2 += st[i + size];
+		i++;
+	}
+	cout << "Итоговая строка: ";
+	cout << *st2;
+	delete[]st2;
+	return 0;
+}
