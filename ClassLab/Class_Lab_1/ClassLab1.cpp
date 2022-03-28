1b830f6736d7e5bb7214601cfe4435b2041f1493
RuslanAkhunov <90615254+FarWG@users.noreply.github.com>
2022-03-28 17:18:56 +0500

    Add files via upload

---
+++ ClassLab/Class_Lab_1/ClassLab1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class ruble
+{
+private:
+	int nominal;
+	int count;
+public:
+	void set(int nominal, int count)
+	{
+		this->nominal = nominal;
+		this->count = count;
+	}
+	int summa()
+	{
+		int sum = 0;
+			sum = nominal * count;
+			return sum;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	ruble a;
+	int nom, cou;
+	cout << "Введите номинал и кол-во купюр этого достоинства:" << endl;
+	cin >> nom;
+	while (nom != 1 && nom != 2 && nom != 5 && nom != 10 && nom != 50 && nom != 100 && nom != 500 && nom != 1000 && nom != 2000 && nom != 5000)
+	{
+		cout << "Вы ввели несуществующий номинал купюры." << endl;
+		cin >> nom;
+	}
+	cin >> cou;
+	a.set(nom, cou);
+	cout << "Денежная сумма: " << a.summa();
+	return 0;
+}
---
+++ ClassLab/Class_Lab_2/ClassLab2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class book
+{
+private:
+	string name;
+	string author;
+	int year;
+public:
+	void set(string name, string author, int year)
+	{
+		this->name = name;
+		this->author = author;
+		this->year = year;
+	}
+	void def()
+	{
+		name = "Дюна";
+		author = "Френк Герберт";
+		year = 1965;
+	}
+	void copy(const book& a)
+	{
+		name = a.name;
+		author = a.author;
+		year = a.year;
+	}
+	void print()
+	{
+		cout << "Название: " << name
+			<< "\nАвтор: " << author
+			<< "\nГод издания: " << year << endl;
+	}
+	~book()
+	{
+		cout << "Сработал Деструктор" << endl;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	book a, b, c;
+	cout << "Конструктор с параметрами" << endl;
+	a.set("Пикник на обочине", "Братья Стругацкие", 1972);
+	a.print();
+	cout << endl;
+	cout << "Конструктор без параметров" << endl;
+	b.def();
+	b.print();
+	cout << endl;
+	cout << "Конструктор копирования" << endl;
+	c.copy(a);
+	c.print();
+	cout << endl;
+	return 0;
+}
